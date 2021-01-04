c8340edc975e678d66455682b170f6cb023bdbbf
Rizki Romadhoni <rrmdn@pm.me>
2021-01-04 14:21:11 +0700

    refactor(cli): add public role to template

--- cli/templates/todo-list-typescript/utils/qoreContext.ts
+++ cli/templates/todo-list-typescript/utils/qoreContext.ts
@@ -4,12 +4,7 @@ import config from "../qore.config.json";
 import schema from "../qore.schema.json";
 import { QoreProjectSchema } from "../qore-generated";
 
-export const client = new QoreClient<QoreProjectSchema>({
-  ...config,
-  getToken: () => {
-    return "ac27480d-3d6e-43ce-a51a-18c97a4a8aae";
-  }
-});
+export const client = new QoreClient<QoreProjectSchema>(config);
 client.init(schema as any);
 
 export default createQoreContext(client);
