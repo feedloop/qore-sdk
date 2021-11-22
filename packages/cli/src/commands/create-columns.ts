8af3caa5c142e04024ad9db4388d6851ef924fe1
syaisyah <terraaisyah@gmail.com>
2021-11-22 10:21:59 +0700

    revamp create-columns in transaction

--- packages/cli/src/commands/create-columns.ts
+++ packages/cli/src/commands/create-columns.ts
@@ -19,34 +19,41 @@ export default class CreateColumn extends Command {
     table: flags.string({ description: "tableName", required: true }),
     columns: flags.string({ description: "columnName:type", required: true })
   };
+
   async run() {
     const { flags } = this.parse(CreateColumn);
     const client = new DefaultApi(
       new Configuration({ apiKey: config.get("apiKey") })
     );
-    this.log(`${chalk.yellow(`\nRunning process ....\n`)}`);
-    const columns = flags.columns.split(",");
-    for (let i = 0; i < columns.length; i++) {
-      const column = columns[i].split(":");
-      const [name, type] = column;
 
-      this.log(`${chalk.grey(`#${i + 1} Create-column ${name}:${type}`)}`);
-      await client.migrate({
-        operations: [
-          {
-            operation: V1MigrateOperationsOperationEnum.Create,
-            resource: V1MigrateOperationsResourceEnum.Column,
-            migration: {
-              name,
-              table: flags.table,
-              column: {
-                type
-              }
-            }
+    const columns = flags.columns.split(",");
+    const operations = [];
+    for (const column of columns) {
+      const [name, type] = column.split(":");
+      operations.push({
+        operation: V1MigrateOperationsOperationEnum.Create,
+        resource: V1MigrateOperationsResourceEnum.Column,
+        migration: {
+          name,
+          table: flags.table,
+          column: {
+            type
           }
-        ]
+        }
       });
     }
+    this.log(`${chalk.yellow(`\nRunning process ....\n`)}`);
+
+    await client.migrate({
+      operations
+    });
+
+    for (let i = 0; i < operations.length; i++) {
+      const { name } = operations[i].migration;
+      const { type } = operations[i].migration.column;
+      this.log(`${chalk.grey(`#${i + 1} Create-column ${name}:${type}`)}`);
+    }
+
     this.log(
       `\n${chalk.grey("\nCreate columns in table")} ${chalk.blue(
         `"${flags.table}"`
